7007009ae8e91c420f3a8ff65688f80e42be89f9
Siam Hossain <siam.hossain1994@gmail.com>
2018-04-01 16:36:55 +0600

    Add textarea

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 if(isset($_POST['submit']))
 $name = $_POST['name'];
+$text = $_POST['text'];
+$add = $name."\n".$text;
 $file = fopen("test.txt","w+");
-fwrite($file, $name);
+fputs($file, $add);
 ?>
 <html>
 <body>
 <form action="#" method = "post">
-<input type =  "text" name = "name">
+Name : 
+<input type =  "text" name = "name"><br><br>
+<textarea rows="4" cols="50" name = "text">
+</textarea><br><br>
 <input type = "submit" name = "submit"> 
+</form>
 </body>
+</html>
